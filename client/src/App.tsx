413dd918da732e093e6b92ccd5dfc4b43f1e878d
ted-jv <wkejrdldls@gmail.com>
2023-01-06 22:34:56 +0900

    Feat: Add NotFound Page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Day from './components/Day';
 import DayList from './components/DayList';
 import Header from './components/Header';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<DayList />}></Route>
           <Route path="/day/:day" element={<Day />}></Route>
-          <Route path="*" element={<>NotFound</>}></Route>
+          <Route path="*" element={<NotFound />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
