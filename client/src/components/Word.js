37e462e89676045f24c0fef6dfeedf9d19ba030e
ted-jv <wkejrdldls@gmail.com>
2023-01-07 22:02:33 +0900

    Feat: Add toggleDone Function
    단어 체크 시 words.isDone 불린값 반대로 변경 기능

--- client/src/components/Word.js
+++ client/src/components/Word.js
@@ -2,17 +2,33 @@ import React, { useState } from 'react';
 
 const Word = ({ word }) => {
   const [isShow, setIsShow] = useState(false);
-  const [isDone, setIsDone] = useState(false);
+  const [isDone, setIsDone] = useState(word.isDone);
+
   const toggleShow = () => {
     setIsShow(!isShow);
   };
+
   const toggleDone = () => {
-    setIsDone(!isDone);
+    fetch(`http://localhost:3001/words/${word.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        ...word,
+        isDone: !isDone,
+      }),
+    }).then(res => {
+      if (res.ok) {
+        setIsDone(!isDone);
+      }
+    });
   };
+
   return (
     <tr className={isDone ? 'off' : ''}>
       <td>
-        <input type="checkbox" onChange={toggleDone} />
+        <input type="checkbox" checked={isDone} onChange={toggleDone} />
       </td>
       <td>{isShow && word.kor}</td>
       <td>{word.eng}</td>
